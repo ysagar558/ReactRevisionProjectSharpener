dd08ebbb0c4db9866d4a594745a7fa4dd073aefb
ysagar558 <ysagar558@gmail.com>
2025-11-13 21:45:10 +0530

    Collecting user i/p using useSate hooks

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,23 +1,35 @@
-// Write your code at relevant places in the code below:
-
-import React from "react";
-import "./AddUser.css";
+import React,{useState} from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
+import "./AddUser.css";
 
 const AddUser = () => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
+
+  const usernameChangeHandler = (event) => {
+    setEnteredUsername(event.target.value);
+  }
+
+  const ageChangeHandler = (event) => {
+    setEnteredAge(event.target.value);
+  }
+
   const addUserHandler = (event) => {
     event.preventDefault();
+    console.log(enteredUsername,enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
   };
 
   return (
-    <Card className="input" >
-    <form onSubmit={addUserHandler}>
-      <label htmlFor="username">Username</label>
-      <input id="username" type="text" />
-      <label htmlFor="age">Age</label>
-      <input id="age" type="number" />
-      <Button type="submit">Add User</Button>
+    <Card className="input">
+      <form onSubmit={addUserHandler}>
+        <label htmlFor="username">Username</label>
+        <input id="username" type="text" value={enteredUsername} onChange={usernameChangeHandler} />
+        <label htmlFor="age">Age</label>
+        <input id="age" type="number" value={enteredAge} onChange={ageChangeHandler} />
+        <Button type="submit">Add User</Button>
       </form>
     </Card>
   );
