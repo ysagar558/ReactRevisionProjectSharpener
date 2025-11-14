5462248af0822a205245b718bb0046f80a7eebe8
ysagar558 <ysagar558@gmail.com>
2025-11-14 14:13:56 +0530

    Rendering users to user list

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 // Write your code at relevant places in the code below:
 
 import React from "react";
-import AddUser from "./components/Users/AddUser"
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsresList";
 
 function App() {
   return (
     <div>
     <AddUser/>
+    <UsersList users={[]}/>
 
     </div>
   );
